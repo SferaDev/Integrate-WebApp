f9403b7ddb15859e10a433de7166a5eff488f55e
Alexis Rico <SferaDev@users.noreply.github.com>
2018-05-04 09:10:52 +0200

    feature/2: Allow entities to login (#3) (#6) (#8) (#10)
    * Login route, reducer, actions & API call
    
    * Login API call
    
    * Fixed API auth & redirect
    
    * fix/node: Force V9 version (#6)
    
    * fix/heroku: Add app.json (#8)
    
    * login middleware
    
    * fixed localstorage test bug

---
+++ src/actions/auth.js
@@ -0,0 +1,48 @@
+import {apiPostLogin} from '../api';
+import {
+    SET_LOGIN_ERROR,
+    SET_LOGIN_PENDING,
+    SET_LOGIN_SUCCESS
+} from '../constants/index';
+
+
+export function setLoginPending(isLoginPending) {
+    return {
+        type: SET_LOGIN_PENDING,
+        isLoginPending
+    };
+}
+
+export function setLoginSuccess(isLoginSuccess) {
+    return {
+        type: SET_LOGIN_SUCCESS,
+        isLoginSuccess
+    };
+}
+
+export function setLoginError(loginError) {
+    return {
+        type: SET_LOGIN_ERROR,
+        loginError
+    }
+}
+
+export function loginAction(id, password) {
+    return dispatch => {
+        dispatch(setLoginPending(true));
+        dispatch(setLoginSuccess(false));
+        dispatch(setLoginError(null));
+
+        apiPostLogin({id, password})
+            .then(token => {
+                localStorage.setItem('token', token);
+                dispatch(setLoginPending(false));
+                dispatch(setLoginSuccess(true));
+            })
+            .catch(error => {
+                dispatch(setLoginPending(false));
+                dispatch(setLoginSuccess(false));
+                dispatch(setLoginError(error))
+            })
+    }
+}

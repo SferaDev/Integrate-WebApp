94283659576b80eaf954896c07369a3d6527922f
Alexis Rico <SferaDev@users.noreply.github.com>
2018-05-04 21:28:54 +0200

    fix/dependencies: Use npx to build project (#15)
    * Fixed dependency issues
    
    * added enzyme

--- src/components/Good/index.js
+++ src/components/Good/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Col, Container, Row,} from 'reactstrap';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import './style.css'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import './style.css';
 
 const Good = ({good, actions}) => {
     const toggle = () => {

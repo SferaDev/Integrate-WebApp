e44015aee49e92002bdc19b6fc7d3a76d4eda69f
Alex Serra Vidal <alex.serra.vidal@est.fib.upc.edu>
2018-05-04 09:48:49 +0200

    feature/6: Good models (#11)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import {render} from 'react-dom';
-import Login from './containers/Login';
 import {Provider} from 'react-redux';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import fontawesome from '@fortawesome/fontawesome'
+import faCoffee from '@fortawesome/fontawesome-free-solid/faCoffee'
+import faPlusCircle from '@fortawesome/fontawesome-free-solid/faPlusCircle'
+import faEdit from '@fortawesome/fontawesome-free-solid/faEdit'
+import faTrashAlt from '@fortawesome/fontawesome-free-solid/faTrashAlt'
+
 import store from './store';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Login from './containers/Login';
+import GoodsContainer from './containers/GoodsContainer'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
+
+fontawesome.library.add(faCoffee, faPlusCircle, faEdit, faTrashAlt)
 
 render(
     <Provider store={store}>
@@ -14,6 +25,7 @@ render(
             <Switch>
                 <Route exact path="/" render={() => <h1>Main Page</h1>}/>
                 <Route path="/login" component={Login}/>
+                <Route path='/goods' component={GoodsContainer}/>
             </Switch>
         </Router>
     </Provider>,
---
+++ src/reducers/goods.js
@@ -0,0 +1,49 @@
+import {ADD_GOOD, DELETE_GOOD, EDIT_GOOD} from '../constants/ActionTypes';
+import * as goodsMock from '../api/mock/goods'
+
+const initialState = goodsMock
+
+export default function goods(state = initialState, action) {
+    switch (action.type) {
+        case ADD_GOOD:
+            return [
+                ...state,
+                {
+                    id: state.reduce((maxId, good) => Math.max(good.id, maxId), -1) + 1,
+                    productName: action.good.productName,
+                    picture: action.good.picture,
+                    initialPrice: action.good.initialPrice,
+                    discountType: action.good.discountType,
+                    discount: action.good.discount,
+                    category: action.good.category,
+                    reusePeriod: action.good.reusePeriod,
+                    pendingUnits: action.good.pendingUnits,
+                }
+            ]
+
+        case DELETE_GOOD:
+            return state.filter(good =>
+                good.id !== action.good.id
+            )
+
+        case EDIT_GOOD:
+            return state.map(good =>
+                good.id === action.good.id ?
+                    {
+                        ...good,
+                        productName: action.good.productName,
+                        picture: action.good.picture,
+                        initialPrice: action.good.initialPrice,
+                        discountType: action.good.discountType,
+                        discount: action.good.discount,
+                        category: action.good.category,
+                        reusePeriod: action.good.reusePeriod,
+                        pendingUnits: action.good.pendingUnits,
+                    } :
+                    good
+            )
+
+        default:
+            return state
+    }
+}

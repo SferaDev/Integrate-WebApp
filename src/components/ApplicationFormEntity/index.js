3a19205033635a4eb74559a792502df9d549e0aa
Alexis Rico <sferadev@gmail.com>
2018-05-04 10:08:06 +0200

    feature/4: Maps and location

--- src/components/ApplicationFormEntity/index.js
+++ src/components/ApplicationFormEntity/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './style.css';
 import {Maps} from "../Maps";
+import {FormGroup, Modal, FormText, Button, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
 
 export class ApplicationForm extends Component {
     constructor(props) {
@@ -15,7 +16,10 @@ export class ApplicationForm extends Component {
             name: '',
             addressName: '',
             addressLatitude: '',
-            addressLongitude: ''
+            addressLongitude: '',
+            modal: false,
+            modalHeader: '',
+            modalContent: ''
         };
         this.changeFirstName = this.changeFirstName.bind(this);
         this.changeLastName = this.changeLastName.bind(this);
@@ -27,8 +31,11 @@ export class ApplicationForm extends Component {
         this.onSentClicked = this.onSentClicked.bind(this);
         this.onCloseClicked = this.onCloseClicked.bind(this);
         this.onUserSearched = this.onUserSearched.bind(this);
+        this.checkEmptyInputs = this.checkEmptyInputs.bind(this);
+        this.checkPhone = this.checkPhone.bind(this);
+        this.checkEmail = this.checkEmail.bind(this);
+        this.checkNIF = this.checkNIF.bind(this);
     }
-
 
     changeFirstName(event) {
         this.setState({salesmanFirstName: event.target.value});
@@ -58,16 +65,81 @@ export class ApplicationForm extends Component {
         this.setState({description: event.target.value});
     }
 
-    onSentClicked(event) {
-        if (this.state.salesmanFirstName === '' || this.state.salesmanLastName === '' || this.state.email === '' || this.state.nif === '' || this.state.name === '' || this.state.addressName === '' || this.state.description === '' || this.state.phone === '') {
-            document.getElementById("TextError").style.visibility = "visible";
+    checkEmail() {
+        let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(this.state.email).toLowerCase());
+    }
+
+    checkPhone() {
+        let i;
+        let count = 0;
+        for (i = 0; i < this.state.phone.length; i++) {
+            if (this.state.phone[i] >= 0 && this.state.phone[i] <= 9) ++count;
         }
-        else {
-            event.preventDefault();
-            console.log(this.state)
+        return (count === 9 && this.state.phone.length === 9);
+    }
+
+    checkNIF() {
+        let nif = this.state.nif.substring(0, this.state.nif.length - 1);
+        let letra = this.state.nif.charAt(this.state.nif.length - 1);
+        if (!isNaN(letra)) {
+            return false;
+        } else {
+            let cadena = "TRWAGMYFPDXBNJZSQVHLCKET";
+            let posicion = nif % 23;
+            letra = cadena.substring(posicion, posicion + 1);
+            let i;
+            let count = 0;
+            for (i = 0; i < this.state.phone.length; i++) {
+                if (this.state.nif[i] >= 0 && this.state.nif[i] <= 9) ++count;
+            }
+            if (letra !== letra.toUpperCase() || (nif.length !== 8 || count !== 8)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    checkEmptyInputs() {
+        return (this.state.salesmanFirstName.length === 0 || this.state.salesmanLastName.length === 0 || this.state.email.length === 0 || this.state.nif.length === 0 || this.state.name.length === 0 || this.state.addressName.length === 0 || this.state.description.length === 0 || this.state.phone.length === 0);
+    }
+
+    onSentClicked(event) {
+        this.setState({
+            modal: !this.state.modal
+        });
+        if (this.state.salesmanFirstName === '' || this.state.salesmanLastName === '' || this.state.email === '' || this.state.nif === '' || this.state.name === '' || this.state.addressName === '' || this.state.description === '' || this.state.phone === '') {
+            // TODO: Check this if condition
+            document.getElementById("TextError").style.visibility = "visible";
+        } else if (this.checkEmptyInputs()) {
+            this.setState({modalHeader: "Error"});
+            this.setState({modalContent: "S'han d'omplir totes les dades del formulari."});
+        } else if (!this.checkEmail()) {
+            this.setState({modalHeader: "Error"});
+            this.setState({modalContent: "L'email introduït no és correcte."});
+        } else if (!this.checkPhone()) {
+            this.setState({modalHeader: "Error"});
+            this.setState({modalContent: "El telèfon introduït no és correcte."});
+        } else if (!this.checkNIF()) {
+            this.setState({modalHeader: "Error"});
+            this.setState({modalContent: "El nif introduït no és correcte."});
+        } else {
+            this.setState({modalHeader: "Correcte"});
+            this.setState({modalContent: "Les dades introduïdes són correctes."});
+            const entity = {
+                salesmanFirstName: this.state.salesmanFirstName,
+                salesmanLastName: this.state.salesmanLastName,
+                nif: this.state.nif,
+                email: this.state.email,
+                phone: this.state.phone,
+                description: this.state.description,
+                name: this.state.name,
+                addressName: this.state.addressName,
+                location: [this.state.addressLongitude, this.state.addressLatitude]
+            }
+            console.log(entity)
             //const {history} = this.props;
             //history.push('/');
-
         }
     }
 
@@ -82,19 +154,16 @@ export class ApplicationForm extends Component {
         this.setState({addressName: address});
         this.setState({addressLatitude: lat});
         this.setState({addressLongitude: lng});
-
-
     }
 
-
     render() {
-
         return (
             <div className="Fons row">
                 <div className="Form col-md-6">
                     <h1 className="HeaderForm">Formulari de sol·licitud</h1>
                     <button className="closeButton" onClick={this.onCloseClicked}><h3>×</h3></button>
                     <hr className="MainLine"/>
+/** TODO
                     <input type="text" className="FirstNameText" placeholder="Nom *"
                            value={this.state.salesmanFirstName} onChange={this.changeFirstName}></input>
                     <input type="text" className="SecondNameText" placeholder="Cognoms *"
@@ -120,6 +189,60 @@ export class ApplicationForm extends Component {
                     </button>
                     <p className="Error" id="TextError" align="center">S'han d'emplenar tots els camps!</p>
                 </div>
+**/
+                    <FormGroup className="FirstFormGroup">
+                        <input type="text" className="FirstNameText" placeholder="Nom *"
+                               value={this.state.salesmanFirstName} onChange={this.changeFirstName}/>
+                    </FormGroup>
+                    <FormGroup className="SecondFormGroup">
+                        <input type="text" className="SecondNameText" placeholder="Cognoms *"
+                               value={this.state.salesmanLastName} onChange={this.changeLastName}/>
+                    </FormGroup>
+                    <FormGroup className="EmailForm">
+                        <input type="email" className="EmailNameText" placeholder="Email *" value={this.state.email}
+                               onChange={this.changeEmail}/>
+                        <FormText>email@example.com</FormText>
+                    </FormGroup>
+                    <FormGroup className="NumberForm">
+                        <input type="text" className="NumberText" placeholder="Telèfon *" value={this.state.phone}
+                               onChange={this.changePhone}/>
+
+                    </FormGroup>
+                    <FormGroup className="EntityForm">
+                        <input type="text" className="EmailNameText" placeholder="Nom de l'entitat *"
+                               value={this.state.name}
+                               onChange={this.changeNameEntity}/>
+                    </FormGroup>
+                    <FormGroup className="NifForm">
+                        <input type="text" className="NumberText" placeholder="NIF *"
+                               value={this.state.nif}
+                               onChange={this.changeNif}/>
+                        <FormText>Ex. format: 60250886G</FormText>
+                    </FormGroup>
+                    <FormGroup className="DescriptionForm">
+                        <textarea type="text" className="DescriptionText" placeholder="Descripció de l'entitat *"
+                                  value={this.state.description} onChange={this.changeDescription}/>
+                    </FormGroup>
+                    <FormGroup className="AddressForm">
+                        <input id="addressId" type="text" className="TextForm" placeholder="Direcció de l'entitat *"
+                               value={this.state.addressName} readOnly/>
+                        <FormText>Escriu l'adreça en el buscador del mapa</FormText>
+                    </FormGroup>
+                    <FormGroup align="center">
+                        <button className="Button" onClick={this.onSentClicked}>
+                            Enviar
+                        </button>
+                    </FormGroup>
+                </div>
+                <Modal isOpen={this.state.modal} toggle={this.onSentClicked} className={this.props.className}>
+                    <ModalHeader toggle={this.onSentClicked}>{this.state.modalHeader}</ModalHeader>
+                    <ModalBody>
+                        {this.state.modalContent}
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={this.onSentClicked}>Acceptar</Button>{' '}
+                    </ModalFooter>
+                </Modal>
                 <div className="Map col-sm-6">
                     <Maps onUserSearched={this.onUserSearched}/>
                 </div>
---
+++ src/components/ApplicationFormEntity/index.spec.js
@@ -0,0 +1,215 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import {expect} from 'chai';
+import {ApplicationForm} from './index';
+import {Maps} from "../Maps";
+
+describe('<ApplicationForm />', () => {
+    it('renders a <Maps /> components', () => {
+        const wrapper = shallow(<ApplicationForm/>);
+        expect(wrapper.find(Maps)).length(1);
+    });
+
+
+    it('should render a modal with an error message explaining that there are empty fields', () => {
+        const wrapper = shallow(<ApplicationForm/>);
+        const sendButton = wrapper.find('button').at(1);
+        sendButton.simulate('click');
+        /* Empty fields */
+        expect(wrapper.state().modalHeader).to.equal('Error');
+        expect(wrapper.state().modalContent).to.equal("S'han d'omplir totes les dades del formulari.");
+        expect(wrapper.state().modal).to.equal(true);
+
+        /* addressName is missing */
+
+        wrapper.setState({salesmanFirstName: 'Pau'});
+        wrapper.setState({salesmanLastName: 'Gonzalez Montiel'});
+        wrapper.setState({nif: '12345678g'});
+        wrapper.setState({email: 'paugonzalez@gmail.com'});
+        wrapper.setState({phone: '634384789'});
+        wrapper.setState({name: 'Pa el fornet'});
+        wrapper.setState({description: 'Venda de pa'});
+        wrapper.setState({modal: false});
+        sendButton.simulate('click');
+        expect(wrapper.state().modalHeader).to.equal('Error');
+        expect(wrapper.state().modalContent).to.equal("S'han d'omplir totes les dades del formulari.");
+        expect(wrapper.state().modal).to.equal(true);
+
+        /* description is missing */
+
+
+        wrapper.setState({salesmanFirstName: 'Pau'});
+        wrapper.setState({salesmanLastName: 'Gonzalez Montiel'});
+        wrapper.setState({nif: '12345678g'});
+        wrapper.setState({email: 'paugonzalez@gmail.com'});
+        wrapper.setState({phone: '634384789'});
+        wrapper.setState({name: 'Pa el fornet'});
+        wrapper.setState({description: ''});
+        wrapper.setState({addressName: 'Avinguda Diagonal, 633, 08028 Barcelona'});
+        wrapper.setState({modal: false});
+        sendButton.simulate('click');
+        expect(wrapper.state().modalHeader).to.equal('Error');
+        expect(wrapper.state().modalContent).to.equal("S'han d'omplir totes les dades del formulari.");
+        expect(wrapper.state().modal).to.equal(true);
+
+        /* salesFirstName and salesLastName are missing */
+
+
+        wrapper.setState({salesmanFirstName: ''});
+        wrapper.setState({salesmanLastName: ''});
+        wrapper.setState({nif: '12345678g'});
+        wrapper.setState({email: 'paugonzalez@gmail.com'});
+        wrapper.setState({phone: '634384789'});
+        wrapper.setState({name: 'Pa el fornet'});
+        wrapper.setState({description: 'Venda de pa'});
+        wrapper.setState({addressName: 'Avinguda Diagonal, 633, 08028 Barcelona'});
+        wrapper.setState({modal: false});
+        sendButton.simulate('click');
+        expect(wrapper.state().modalHeader).to.equal('Error');
+        expect(wrapper.state().modalContent).to.equal("S'han d'omplir totes les dades del formulari.");
+        expect(wrapper.state().modal).to.equal(true);
+
+    });
+
+    it('should render a modal with an error message explaining that the phone number is incorrect', () => {
+        const wrapper = shallow(<ApplicationForm/>);
+        const sendButton = wrapper.find('button').at(1);
+
+        /*it has a character*/
+        wrapper.setState({salesmanFirstName: 'Pau'});
+        wrapper.setState({salesmanLastName: 'Gonzalez Montiel'});
+        wrapper.setState({nif: '12345678g'});
+        wrapper.setState({email: 'paugonzalez@gmail.com'});
+        wrapper.setState({phone: '62864208p'});
+        wrapper.setState({name: 'Pa el fornet'});
+        wrapper.setState({description: 'Venda de pa'});
+        wrapper.setState({addressName: 'Avinguda Diagonal, 633, 08028 Barcelona'});
+        sendButton.simulate('click');
+        expect(wrapper.state().modalHeader).to.equal('Error');
+        expect(wrapper.state().modalContent).to.equal("El telèfon introduït no és correcte.");
+        expect(wrapper.state().modal).to.equal(true);
+
+        /*Only 8 numbers*/
+        wrapper.setState({salesmanFirstName: 'Pau'});
+        wrapper.setState({salesmanLastName: 'Gonzalez Montiel'});
+        wrapper.setState({nif: '12345678g'});
+        wrapper.setState({email: 'paugonzalez@gmail.com'});
+        wrapper.setState({phone: '62864208'});
+        wrapper.setState({name: 'Pa el fornet'});
+        wrapper.setState({description: 'Venda de pa'});
+        wrapper.setState({addressName: 'Avinguda Diagonal, 633, 08028 Barcelona'});
+        wrapper.setState({modal: false});
+        sendButton.simulate('click');
+        expect(wrapper.state().modalHeader).to.equal('Error');
+        expect(wrapper.state().modalContent).to.equal("El telèfon introduït no és correcte.");
+        expect(wrapper.state().modal).to.equal(true);
+
+    });
+
+    it('should render a modal with an error message explaining that the email is incorrect', () => {
+        const wrapper = shallow(<ApplicationForm/>);
+        const sendButton = wrapper.find('button').at(1);
+
+        wrapper.setState({salesmanFirstName: 'Pau'});
+        wrapper.setState({salesmanLastName: 'Gonzalez Montiel'});
+        wrapper.setState({nif: '12345678g'});
+        wrapper.setState({email: 'paugonzalez@gmailom'});
+        wrapper.setState({phone: '628642082'});
+        wrapper.setState({name: 'Pa el fornet'});
+        wrapper.setState({description: 'Venda de pa'});
+        wrapper.setState({addressName: 'Avinguda Diagonal, 633, 08028 Barcelona'});
+        sendButton.simulate('click');
+        expect(wrapper.state().modalHeader).to.equal('Error');
+        expect(wrapper.state().modalContent).to.equal("L'email introduït no és correcte.");
+        expect(wrapper.state().modal).to.equal(true);
+
+
+        wrapper.setState({salesmanFirstName: 'Pau'});
+        wrapper.setState({salesmanLastName: 'Gonzalez Montiel'});
+        wrapper.setState({nif: '12345678g'});
+        wrapper.setState({email: 'paugonzalezgmail.com'});
+        wrapper.setState({phone: '628642082'});
+        wrapper.setState({name: 'Pa el fornet'});
+        wrapper.setState({description: 'Venda de pa'});
+        wrapper.setState({addressName: 'Avinguda Diagonal, 633, 08028 Barcelona'});
+        wrapper.setState({modal: false});
+        sendButton.simulate('click');
+        expect(wrapper.state().modalHeader).to.equal('Error');
+        expect(wrapper.state().modalContent).to.equal("L'email introduït no és correcte.");
+        expect(wrapper.state().modal).to.equal(true);
+
+    });
+
+    it('should render a modal with an error message explaining that the nif is incorrect', () => {
+        const wrapper = shallow(<ApplicationForm/>);
+        const sendButton = wrapper.find('button').at(1);
+
+        /*it has only 6 numbers*/
+
+        wrapper.setState({salesmanFirstName: 'Pau'});
+        wrapper.setState({salesmanLastName: 'Gonzalez Montiel'});
+        wrapper.setState({nif: '123456g'});
+        wrapper.setState({email: 'paugonzalez@gmail.com'});
+        wrapper.setState({phone: '628642082'});
+        wrapper.setState({name: 'Pa el fornet'});
+        wrapper.setState({description: 'Venda de pa'});
+        wrapper.setState({addressName: 'Avinguda Diagonal, 633, 08028 Barcelona'});
+        sendButton.simulate('click');
+        expect(wrapper.state().modalHeader).to.equal('Error');
+        expect(wrapper.state().modalContent).to.equal("El nif introduït no és correcte.");
+        expect(wrapper.state().modal).to.equal(true);
+
+        /*it doesn't have a character*/
+        wrapper.setState({salesmanFirstName: 'Pau'});
+        wrapper.setState({salesmanLastName: 'Gonzalez Montiel'});
+        wrapper.setState({nif: '123456789'});
+        wrapper.setState({email: 'paugonzalez@gmail.com'});
+        wrapper.setState({phone: '628642082'});
+        wrapper.setState({name: 'Pa el fornet'});
+        wrapper.setState({description: 'Venda de pa'});
+        wrapper.setState({addressName: 'Avinguda Diagonal, 633, 08028 Barcelona'});
+        wrapper.setState({modal: false});
+        sendButton.simulate('click');
+        expect(wrapper.state().modalHeader).to.equal('Error');
+        expect(wrapper.state().modalContent).to.equal("El nif introduït no és correcte.");
+        expect(wrapper.state().modal).to.equal(true);
+
+    });
+
+    it('should render a modal with a message explaining the data is correct', () => {
+        const wrapper = shallow(<ApplicationForm/>);
+        const sendButton = wrapper.find('button').at(1);
+
+
+        wrapper.setState({salesmanFirstName: 'Pau'});
+        wrapper.setState({salesmanLastName: 'Gonzalez Montiel'});
+        wrapper.setState({nif: '12345678g'});
+        wrapper.setState({email: 'paugonzalez@gmail.com'});
+        wrapper.setState({phone: '628642082'});
+        wrapper.setState({name: 'Pa el fornet'});
+        wrapper.setState({description: 'Venda de pa'});
+        wrapper.setState({addressName: 'Avinguda Diagonal, 633, 08028 Barcelona'});
+        sendButton.simulate('click');
+        expect(wrapper.state().modalHeader).to.equal('Correcte');
+        expect(wrapper.state().modalContent).to.equal("Les dades introduïdes són correctes.");
+        expect(wrapper.state().modal).to.equal(true);
+
+        /*it doesn't have a character*/
+        wrapper.setState({salesmanFirstName: 'Pau'});
+        wrapper.setState({salesmanLastName: 'Gonzalez Montiel'});
+        wrapper.setState({nif: '12345678g'});
+        wrapper.setState({email: 'paugonzalez@gmail.com'});
+        wrapper.setState({phone: '628642082'});
+        wrapper.setState({name: 'Pa el fornet'});
+        wrapper.setState({description: 'Venda de pa'});
+        wrapper.setState({addressName: 'Avinguda Diagonal, 633, 08028 Barcelona'});
+        wrapper.setState({modal: false});
+        sendButton.simulate('click');
+        expect(wrapper.state().modalHeader).to.equal('Correcte');
+        expect(wrapper.state().modalContent).to.equal("Les dades introduïdes són correctes.");
+        expect(wrapper.state().modal).to.equal(true);
+
+    });
+
+
+});

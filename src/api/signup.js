7e469cd18783dc9d0411b4d7626ada57c80aa789
paugm96 <pau.gonzalez.montiel@est.fib.upc.edu>
2018-05-05 21:22:36 +0200

    api signup

---
+++ src/api/signup.js
@@ -0,0 +1,23 @@
+import {post} from 'axios';
+import {API_HOST} from './';
+
+
+export const apiPostSignUp = (entity) =>
+    new Promise((resolve, reject) => {
+        post(`${API_HOST}/register`, entity).then(response => {
+            console.log(response)
+        })
+            .catch(error => {
+                console.log(error.response)
+            })
+    })
+            /*nif: "",
+            email: "",
+            salesmanFirstName: "",
+            salesmanLastName: "",
+            name: "",
+            description: "",
+            addressName: "",
+            coordinates: "",
+            phone: ""*/
+            //)})
--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import './style.css';
 import {Maps} from "../Maps";
+import {apiPostSignUp} from "../../api/signup";
+import {FormGroup, Modal, FormText, Button, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
 
 export default class SignUp extends Component {
     constructor(props) {
@@ -15,7 +17,10 @@ export default class SignUp extends Component {
             name: '',
             addressName: '',
             addressLatitude: '',
-            addressLongitude: ''
+            addressLongitude: '',
+            modal: false,
+            modalHeader: '',
+            modalContent: ''
         };
         this.changeFirstName = this.changeFirstName.bind(this);
         this.changeLastName = this.changeLastName.bind(this);
@@ -27,6 +32,11 @@ export default class SignUp extends Component {
         this.onSentClicked = this.onSentClicked.bind(this);
         this.onCloseClicked = this.onCloseClicked.bind(this);
         this.onUserSearched = this.onUserSearched.bind(this);
+        this.checkEmptyInputs = this.checkEmptyInputs.bind(this);
+        this.checkPhone = this.checkPhone.bind(this);
+        this.checkEmail = this.checkEmail.bind(this);
+        this.checkNIF = this.checkNIF.bind(this);
+        this.onAcceptClicked = this.onAcceptClicked.bind(this)
     }
 
 
@@ -58,13 +68,73 @@ export default class SignUp extends Component {
         this.setState({description: event.target.value});
     }
 
-    onSentClicked(event) {
-        if (this.state.salesmanFirstName === '' || this.state.salesmanLastName === '' || this.state.email === '' || this.state.nif === '' || this.state.name === '' || this.state.addressName === '' || this.state.description === '' || this.state.phone === '') {
-            document.getElementById("TextError").style.visibility = "visible";
+    checkEmail() {
+        let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(this.state.email).toLowerCase());
+    }
+
+    checkPhone() {
+        let i;
+        let count = 0;
+        for (i = 0; i < this.state.phone.length; i++) {
+            if (this.state.phone[i] >= 0 && this.state.phone[i] <= 9) ++count;
         }
+        return (count === 9 && this.state.phone.length === 9);
+    }
+
+    checkNIF() {
+        let nif = this.state.nif.substring(0, this.state.nif.length - 1);
+        let letra = this.state.nif.charAt(this.state.nif.length - 1);
+        if (!isNaN(letra)) {
+            return false;
+        } else {
+            let cadena = "TRWAGMYFPDXBNJZSQVHLCKET";
+            let posicion = nif % 23;
+            letra = cadena.substring(posicion, posicion + 1);
+            let i;
+            let count = 0;
+            for (i = 0; i < this.state.phone.length; i++) {
+                if (this.state.nif[i] >= 0 && this.state.nif[i] <= 9) ++count;
+            }
+            if (letra !== letra.toUpperCase() || (nif.length !== 8 || count !== 8)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    checkEmptyInputs() {
+        return (this.state.salesmanFirstName.length === 0 || this.state.salesmanLastName.length === 0 || this.state.email.length === 0 || this.state.nif.length === 0 || this.state.name.length === 0 || this.state.addressName.length === 0 || this.state.description.length === 0 || this.state.phone.length === 0);
+    }
+
+    onSentClicked(event) {
+        this.setState({
+            modal: !this.state.modal
+        });
+        if (this.checkEmptyInputs()) {
+            this.setState({modalHeader: "Error"});
+            this.setState({modalContent: "S'han d'omplir totes les dades del formulari."});
+        }
+
+        else if (!this.checkEmail()) {
+            this.setState({modalHeader: "Error"});
+            this.setState({modalContent: "L'email introduït no és correcte."});
+        }
+
+        else if (!this.checkPhone()) {
+            this.setState({modalHeader: "Error"});
+            this.setState({modalContent: "El telèfon introduït no és correcte."});
+        }
+
+        else if (!this.checkNIF()) {
+            this.setState({modalHeader: "Error"});
+            this.setState({modalContent: "El nif introduït no és correcte."});
+        }
+
         else {
-            event.preventDefault();
-            console.log(this.state)
+            this.setState({modalHeader: "Correcte"});
+            this.setState({modalContent: "Les dades introduïdes són correctes."});
+
             //const {history} = this.props;
             //history.push('/');
 
@@ -75,6 +145,32 @@ export default class SignUp extends Component {
         event.preventDefault();
         const {history} = this.props;
         history.push('/');
+    }
+
+    onAcceptClicked(event) {
+        if (this.state.modalHeader === "Error") {
+            !this.setState({
+                modal: !this.state.modal
+            });
+        }
+        else {
+            const entity = {
+                salesmanFirstName: this.state.salesmanFirstName,
+                salesmanLastName: this.state.salesmanLastName,
+                nif: this.state.nif,
+                email: this.state.email,
+                phone: this.state.phone,
+                description: this.state.description,
+                name: this.state.name,
+                addressName: this.state.addressName,
+                picture: "hola",
+                coordinates: [this.state.addressLongitude, this.state.addressLatitude]
+            }
+            const coordinates = [this.state.addressLongitude, this.state.addressLatitude]
+            apiPostSignUp(entity)
+            console.log(entity)
+
+        }
     }
 
 
@@ -95,31 +191,59 @@ export default class SignUp extends Component {
                     <h1 className="HeaderForm">Formulari de sol·licitud</h1>
                     <button className="closeButton" onClick={this.onCloseClicked}><h3>×</h3></button>
                     <hr className="MainLine"/>
-                    <input type="text" className="FirstNameText" placeholder="Nom *"
-                           value={this.state.salesmanFirstName} onChange={this.changeFirstName}></input>
-                    <input type="text" className="SecondNameText" placeholder="Cognoms *"
-                           value={this.state.salesmanLastName} onChange={this.changeLastName}></input>
-                    <input type="email" className="EmailNameText" placeholder="Email *" value={this.state.email}
-                           onChange={this.changeEmail}></input>
-                    <input type="text" className="NumberText" placeholder="Telèfon *" value={this.state.phone}
-                           onChange={this.changePhone}></input>
-                    <input type="text" className="EmailNameText" placeholder="Nom de l'entitat *"
-                           value={this.state.name}
-                           onChange={this.changeNameEntity}></input>
-                    <input type="text" className="NumberText" placeholder="NIF *"
-                           value={this.state.nif}
-                           onChange={this.changeNif}></input>
-                    <textarea type="text" className="DescriptionText" placeholder="Descripció de l'entitat *"
-                              value={this.state.description} onChange={this.changeDescription}></textarea>
+                    <FormGroup className="FirstFormGroup">
+                        <input type="text" className="FirstNameText" placeholder="Nom *"
+                               value={this.state.salesmanFirstName} onChange={this.changeFirstName}/>
+                    </FormGroup>
+                    <FormGroup className="SecondFormGroup">
+                        <input type="text" className="SecondNameText" placeholder="Cognoms *"
+                               value={this.state.salesmanLastName} onChange={this.changeLastName}/>
+                    </FormGroup>
+                    <FormGroup className="EmailForm">
+                        <input type="email" className="EmailNameText" placeholder="Email *" value={this.state.email}
+                               onChange={this.changeEmail}/>
+                        <FormText>email@example.com</FormText>
+                    </FormGroup>
+                    <FormGroup className="NumberForm">
+                        <input type="text" className="NumberText" placeholder="Telèfon *" value={this.state.phone}
+                               onChange={this.changePhone}/>
 
-                    <input type="text" className="TextForm" placeholder="Direcció de l'entitat *"
-                           value={this.state.addressName} readOnly></input>
-                    <p className="address">Busca la teva adreça en el mapa</p>
-                    <button className="ButtonForm" onClick={this.onSentClicked}>
-                        Enviar
-                    </button>
-                    <p className="Error" id="TextError" align="center">S'han d'emplenar tots els camps!</p>
+                    </FormGroup>
+                    <FormGroup className="EntityForm">
+                        <input type="text" className="EmailNameText" placeholder="Nom de l'entitat *"
+                               value={this.state.name}
+                               onChange={this.changeNameEntity}/>
+                    </FormGroup>
+                    <FormGroup className="NifForm">
+                        <input type="text" className="NumberText" placeholder="NIF *"
+                               value={this.state.nif}
+                               onChange={this.changeNif}/>
+                        <FormText>Ex. format: 60250886G</FormText>
+                    </FormGroup>
+                    <FormGroup className="DescriptionForm">
+                        <textarea type="text" className="DescriptionText" placeholder="Descripció de l'entitat *"
+                                  value={this.state.description} onChange={this.changeDescription}/>
+                    </FormGroup>
+                    <FormGroup className="AddressForm">
+                        <input id="addressId" type="text" className="TextForm" placeholder="Direcció de l'entitat *"
+                               value={this.state.addressName} readOnly/>
+                        <FormText>Escriu l'adreça en el buscador del mapa</FormText>
+                    </FormGroup>
+                    <FormGroup align="center">
+                        <button className="Button" onClick={this.onSentClicked}>
+                            Enviar
+                        </button>
+                    </FormGroup>
                 </div>
+                <Modal isOpen={this.state.modal} toggle={this.onSentClicked} className={this.props.className}>
+                    <ModalHeader toggle={this.onSentClicked}>{this.state.modalHeader}</ModalHeader>
+                    <ModalBody>
+                        {this.state.modalContent}
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={this.onAcceptClicked}>Acceptar</Button>{' '}
+                    </ModalFooter>
+                </Modal>
                 <div className="Map col-sm-6">
                     <Maps onUserSearched={this.onUserSearched}/>
                 </div>

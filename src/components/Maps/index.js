4e1a3d81e8ac5d20f79485ad3980e4e017d074e3
Alex Serra Vidal <alex.serra.vidal@est.fib.upc.edu>
2018-05-10 21:07:42 +0200

    Environment variables set

--- src/components/Maps/index.js
+++ src/components/Maps/index.js
@@ -8,7 +8,7 @@ import _ from 'lodash';
 
 const MyMapComponent = compose(
     withProps({
-        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places",
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=" + process.env.REACT_APP_GOOGLE_MAPS_API_KEY + "&v=3.exp&libraries=geometry,drawing,places",
         loadingElement: <div style={{height: `100%`}}/>,
         containerElement: <div style={{height: `100%`}}/>,
         mapElement: <div style={{height: `80%`}}/>,

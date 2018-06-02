3d25e9ca19339def4a15cad338547b827ddcb316
Alex Serra Vidal <alex.serra.vidal@est.fib.upc.edu>
2018-06-02 21:19:23 +0200

    Cloudinary API file coverage increased to 100%

--- src/api/__mocks__/index.js
+++ src/api/__mocks__/index.js
@@ -11,3 +11,27 @@ export const putApi = (endpoint, data) => new Promise((resolve, reject) => {
                 }),
     );
 });
+
+export const postCloudinary = (formData) => new Promise((resolve, reject) => {
+    console.log(formData.getObjectName())
+    const mockResponse =
+    formData.getObjectName() === 'FormDataMock' ?
+    {
+        data: {
+            secure_url: 'http://mockedSecureUrl.com/secure.jpg'
+        }
+    } : {
+        data: {
+            secure_url: null
+        }
+    }
+
+    process.nextTick(
+        () =>
+            mockResponse.data.secure_url
+                ? resolve(mockResponse)
+                : reject({
+                errorUploadCloudinary: 'Image could not be uploaded'
+                })
+    )
+})

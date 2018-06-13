400babdc6fb1aecefff9c7cf303ff5204fc79f84
Alex Serra Vidal <alex.serra.vidal@est.fib.upc.edu>
2018-06-13 23:29:42 +0200

    * Set the active Link in the navbar

--- src/containers/ChangePassword/index.js
+++ src/containers/ChangePassword/index.js
@@ -44,7 +44,7 @@ export class ChangePasswordContainer extends Component {
                 <Container fluid={true}>
                     <div className="signupContainer">
                         <LanguageSelector actions={actions.localeActions} lang={lang}/>
-                        <MainView actions={actions.authActions} userName={this.props.auth.user.name}/>
+                        <MainView actions={actions.authActions} userName={this.props.auth.user.name} active="changePassword"/>
                             <ChangePassword history={this.props.history}/>
                     </div>
                 </Container>
